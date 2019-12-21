9a530e0f02af1f0da0b0eebb0b6393fc84405179
Shroedingercat <home31999@gmail.com>
2019-12-21 14:52:39 +0300

    2

---
+++ dz6/cmake-build-debug/CMakeFiles/dz6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dz6.dir/main.cpp.o"
+  "dz6.exe"
+  "dz6.pdb"
+  "libdz6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dz6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
